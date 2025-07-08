7a238ff60e9e9b08a6a86ba60e5d781a59016e4a
mcmellado <celestemellado@iesdonana.org>
2025-07-08 15:37:41 +0200

    cambio de tabla base de datos

--- database/migrations/2025_06_29_124817_create_equipos_table.php
+++ database/migrations/2025_06_29_124817_create_equipos_table.php
@@ -6,28 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('equipos', function (Blueprint $table) {
             $table->id();
             $table->string('numero_serie')->unique();
             $table->string('modelo');
-            $table->foreignId('puesto_actual_id')->constrained('puestos');
-            $table->date('fecha_ingreso')->nulleable();
+
+            $table->foreignId('puesto_actual_id')
+                  ->nullable()
+                  ->constrained('puestos')
+                  ->onDelete('set null');
+
+            $table->foreignId('proveedor_id')
+                  ->nullable()
+                  ->constrained('proveedores')
+                  ->onDelete('set null');
+
+            $table->foreignId('comprador_id')
+                  ->nullable()
+                  ->constrained('compradores')
+                  ->onDelete('set null');
+
+            $table->date('fecha_ingreso')->nullable();
             $table->timestamps(); 
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('equipos');
